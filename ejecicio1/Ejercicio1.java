967b2ab938fae151db61ba9635441d27867e13e0
Aitor <aalopen850@g.educaand.es>
2025-10-04 17:23:56 +0200

    Hola, Mundo!
    Mi primer ejercicio del módulo de Programación

---
+++ ejecicio1/Ejercicio1.java
@@ -0,0 +1,17 @@
+/*
+ * Descripción: Mostrar en consola un texto
+ * Autor: Aitor Alonso
+ * Fecha: 25/09/25
+ */
+package ejecicio1;
+
+public class Ejercicio1 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println ("Este es mi primer programa Java"); //Mostramos 
+
+	}
+
+}
